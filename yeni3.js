949da326342454e5f458f7bb7b07d10c3cdef988
ısmaıl <ismailaz984@gmail.com>
2025-08-25 11:31:53 +0300

    selamlama bir defa olacak şekilde düzenlendi

--- yeni3.js
+++ yeni3.js
@@ -1,11 +1,3 @@
-// Sayfa yüklendiğinde konsola mesaj yaz
-console.log("selam");
-
 document.addEventListener("DOMContentLoaded", function() {
-  if (!localStorage.getItem("welcomed")) {
-    alert("web siteme hoş geldin");
-    localStorage.setItem("welcomed", "true");
-  }
+  alert("web siteme hoş geldin");
 });
-
-
